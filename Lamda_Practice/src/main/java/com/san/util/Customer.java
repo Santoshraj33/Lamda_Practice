cd4a70e55abfa68b1b86bcd90f3373b7e08d3bf2
Santosh <Santosh@192.168.0.6>
2022-01-02 13:22:23 +0530

    Initial Commit

--- Lamda_Practice/src/main/java/com/san/util/Customer.java
+++ Lamda_Practice/src/main/java/com/san/util/Customer.java
@@ -1,8 +1,32 @@
 package com.san.util;
 
 public class Customer {
-	
+
 	private int id;
-	private String name;
+	private String name, city;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
 
 }
