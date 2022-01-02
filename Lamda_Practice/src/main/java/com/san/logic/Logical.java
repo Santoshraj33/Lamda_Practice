daa841f13a98d6a9d5007fae0070a3a3a27fbe07
Santosh <Santosh@192.168.0.6>
2022-01-02 15:09:21 +0530

    Sorted the names

--- Lamda_Practice/src/main/java/com/san/logic/Logical.java
+++ Lamda_Practice/src/main/java/com/san/logic/Logical.java
@@ -1,9 +1,32 @@
 package com.san.logic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.san.util.Customer;
 
 public class Logical {
-	
-	
+	Customer customer, customer1 = null;
+	List<Customer> list = null;
+
+	public List<Customer> getDetails() {
+
+		list = new ArrayList<Customer>();
+		customer = new Customer();
+		customer1 = new Customer();
+
+		customer.setName("Santosh");
+		customer.setId(12);
+		customer.setCity("Hyderabad");
+
+		customer1.setName("Ramcharan");
+		customer1.setId(201);
+		customer1.setCity("Suryapet");
+
+		list.add(customer);
+		list.add(customer1);
+
+		return list;
+	}
 
 }
--- Lamda_Practice/src/main/java/com/san/main/App.java
+++ Lamda_Practice/src/main/java/com/san/main/App.java
@@ -1,13 +1,27 @@
 package com.san.main;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.san.logic.Logical;
+import com.san.util.Customer;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		List<Customer> list = null;
+		Logical logical = new Logical();
+		list = new LinkedList<>();
+
+		list = logical.getDetails();
+		Collections.sort(list, (a, b) -> a.getName().compareToIgnoreCase(b.getName()));
+		for (Customer customer : list) {
+			System.out.println(customer.getName());
+		}
+
+	}
 }
